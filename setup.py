4096d8bb6e51a8cb500c9e0530e0bdeafd1a6000
lnxpy <lnxpylnxpy@gmail.com>
2022-08-13 00:11:29 +0430

    minimun required django version is 2.2

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [
+    'django>=2.2'
+]
 
 test_requirements = [ ]
 
