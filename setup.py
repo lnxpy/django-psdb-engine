d6a1f886e0ec617143307c5a8bf431ead72c0608
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-08-30 01:08:06 +0430

    content_type set to text/markdown

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import find_packages, setup
 
-with open("README.md") as readme_file:
+with open("README.md", "r", encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
 requirements = [
@@ -32,6 +32,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords="django_psdb_engine",
     name="django_psdb_engine",
